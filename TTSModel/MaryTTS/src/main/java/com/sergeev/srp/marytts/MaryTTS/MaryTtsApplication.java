e2b65d1559ef4aa996ac38d06a4263320533c004
Roman Sergeev <sergeev.rs08@gmail.com>
2023-04-17 19:03:04 +0300

    Настройки модулей

---
+++ TTSModel/MaryTTS/src/main/java/com/sergeev/srp/marytts/MaryTTS/MaryTtsApplication.java
@@ -0,0 +1,13 @@
+package com.sergeev.srp.marytts.MaryTTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryTtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaryTtsApplication.class, args);
+	}
+
+}
---
+++ TTSModel/MaryTTS/src/test/java/com/sergeev/srp/marytts/MaryTTS/MaryTtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergeev.srp.marytts.MaryTTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaryTtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

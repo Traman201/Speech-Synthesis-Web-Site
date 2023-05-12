d768af22ce571abe345087f9f423365ae769b02d
Roman Sergeev <sergeev.rs08@gmail.com>
2023-05-12 21:12:00 +0300

    Новые голоса

--- SpeechSite/src/main/resources/data.sql
+++ SpeechSite/src/main/resources/data.sql
@@ -3,8 +3,11 @@ VALUES ('ru', 'Русский'),
        ('en', 'Английский');
 
 INSERT INTO maryttsvoice (name, name_for_site, locale_id)
-VALUES ('dfki-poppy-hsmm', 'Poppy', 'en'),
-       ('cmu-slt-hsmm', 'Standart', 'en');
+VALUES ('dfki-poppy-hsmm', 'Poppy', 'en_US'),
+       ('cmu-slt-hsmm', 'Olivia', 'en_US'),
+       ('ac-irina-hsmm', 'Ирина', 'ru'),
+       ('dfki-spike-hsmm', 'Spike', 'en_US'),
+       ('dfki-prudence-hsmm', 'Prudence', 'en_US');
 
 INSERT INTO hmm_model (name, name_for_site)
 VALUES ('RUSSIAN', 'Русский (Фонетический алфавит)'),
